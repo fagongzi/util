45ec4cc9ef3765247111ffe43cef329c2a8a4ebe
zhangxu19830126@gmail.com <zhangxu19830126@gmail.com>
2017-06-27 09:32:42 +0800

    dev: init util package

---
+++ task/task_test.go
@@ -0,0 +1,165 @@
+package task
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestTask(t *testing.T) {
+	runner := NewRunner()
+	c := make(chan struct{})
+	defer close(c)
+
+	err := runner.RunTask(func() {
+		c <- struct{}{}
+	})
+
+	if err != nil {
+		t.Error("run task failed, return a error", err)
+		return
+	}
+
+	select {
+	case <-c:
+	case <-time.After(time.Millisecond * 50):
+		t.Error("run task failed, task not run after 50ms")
+	}
+
+	runner.AddNamedWorker("name-0", 2)
+	yes := false
+	var ok int32
+	complete := make(chan struct{}, 1)
+	runner.RunJobWithNamedWorker("name-0", func() error {
+		atomic.StoreInt32(&ok, 1)
+		return nil
+	})
+
+	runner.RunJobWithNamedWorker("name-0", func() error {
+		if atomic.LoadInt32(&ok) > 0 {
+			yes = true
+		}
+		complete <- struct{}{}
+		return nil
+	})
+
+	<-complete
+	if !yes {
+		t.Error("run named task failed, task not liner excution")
+	}
+
+	start := make(chan struct{}, 1)
+	defer close(start)
+
+	result := false
+
+	_, err = runner.RunCancelableTask(func(c context.Context) {
+		select {
+		case <-c.Done():
+			result = true
+		case <-start:
+
+		}
+	})
+
+	if err != nil {
+		t.Error("run cancelable task failed, return a error", err)
+		return
+	}
+
+	defaultWaitStoppedTimeout = time.Second
+	err = runner.Stop()
+	if err != nil {
+		t.Error("stop runner failed, return a error", err)
+		return
+	}
+
+	start <- struct{}{}
+	if !result {
+		t.Error("cancelable task excuted after stop")
+		return
+	}
+}
+
+func TestNamedTask(t *testing.T) {
+	runner := NewRunner()
+	runner.AddNamedWorker("apply", 64)
+
+	cnt := 0
+	complete := make(chan struct{}, 1)
+	runner.RunJobWithNamedWorker("apply", func() error {
+		cnt++
+		complete <- struct{}{}
+		return nil
+	})
+
+	<-complete
+	if cnt != 1 {
+		t.Errorf("run named job failed. expect=<%d>, actual=<%d>", 1, cnt)
+	}
+
+	runner.RunJobWithNamedWorker("apply", func() error {
+		cnt++
+		complete <- struct{}{}
+		return nil
+	})
+
+	<-complete
+	if cnt != 2 {
+		t.Errorf("run named job failed. expect=<%d>, actual=<%d>", 2, cnt)
+	}
+}
+
+func TestStopWithID(t *testing.T) {
+	runner := NewRunner()
+	c := make(chan struct{})
+	defer close(c)
+
+	result := false
+	start := make(chan struct{}, 1)
+	defer close(start)
+
+	id, err := runner.RunCancelableTask(func(c context.Context) {
+		select {
+		case <-c.Done():
+			result = true
+		case <-start:
+
+		}
+	})
+	if err != nil {
+		t.Error("run cancelable task failed, return a error", err)
+		return
+	}
+
+	err = runner.StopCancelableTask(id)
+	if err != nil {
+		t.Error("stop cancelable task failed, return a error", err)
+		return
+	}
+
+	for index := 0; index < 10; index++ {
+		runner.RunCancelableTask(func(c context.Context) {
+			select {
+			case <-c.Done():
+				result = true
+			case <-time.After(time.Hour):
+
+			}
+		})
+	}
+
+	defaultWaitStoppedTimeout = time.Second
+	err = runner.Stop()
+	if err != nil {
+		t.Error("stop runner failed, return a error", err)
+		return
+	}
+
+	start <- struct{}{}
+	if !result {
+		t.Error("cancelable task excuted after stop")
+		return
+	}
+}
