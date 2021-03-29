fd7f5ad7adf7e8598c6669d8d7a0c43c2af7619d
zhangxu <zhangxu19830126@gmail.com>
2021-03-29 11:46:13 +0800

    add tuple

---
+++ tuple/uint64.go
@@ -0,0 +1,66 @@
+package Tuple
+
+// ByteTuple byte tuple
+type ByteTuple struct {
+	First, Second byte
+}
+
+// BytesTuple bytes tuple
+type BytesTuple struct {
+	First, Second []byte
+}
+
+// UintTuple uint tuple
+type UintTuple struct {
+	First, Second uint
+}
+
+// Uint8Tuple uint8 tuple
+type Uint8Tuple struct {
+	First, Second uint8
+}
+
+// Uint16Tuple uint16 tuple
+type Uint16Tuple struct {
+	First, Second uint16
+}
+
+// Uint32Tuple uint32 tuple
+type Uint32Tuple struct {
+	First, Second uint32
+}
+
+// Uint64Tuple uint64 tuple
+type Uint64Tuple struct {
+	First, Second uint64
+}
+
+// IntTuple int tuple
+type IntTuple struct {
+	First, Second int
+}
+
+// Int8Tuple int8 tuple
+type Int8Tuple struct {
+	First, Second int8
+}
+
+// Int16Tuple int16 tuple
+type Int16Tuple struct {
+	First, Second int16
+}
+
+// Int32Tuple int32 tuple
+type Int32Tuple struct {
+	First, Second int32
+}
+
+// Int64Tuple int64 tuple
+type Int64Tuple struct {
+	First, Second int64
+}
+
+// Int64Tuple string tuple
+type StringTuple struct {
+	First, Second string
+}
